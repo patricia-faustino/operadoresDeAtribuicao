0ad8fdaa76e25b51d4819007d5e63a9f68cb4a87
Patricia <patriciafaustinobr@gmail.com>
2020-09-27 19:40:09 -0300

    Atribuindo valores a variaveis com operadores de atribuicao

---
+++ OPERADORES DE ATRIBUIÇÃO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OperadoresDeAtribuicao {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+            a -= 2;
+            Console.WriteLine(a);
+            a *= 3;
+            Console.WriteLine(a);
+            string s = "ABS";
+            Console.WriteLine(s);
+            s += "DEv";
+            Console.WriteLine(s);
+
+
+        }
+    }
+}
